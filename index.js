150eda388b2eafcbdde67c213c2527c5db36fc7c
Kristea Gamel <krgamel@gmail.com>
2019-06-11 20:00:48 -0500

    server is up and running
    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const PORT = process.env.PORT || 6000;
+
+server.listen(PORT, () =>
+console.log(`Server Running on Port:${PORT}`));
