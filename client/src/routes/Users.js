8009bee5f70b3a5e71e4becdf5935c1c7b36c7a0
Kristea Gamel <krgamel@gmail.com>
2019-06-13 21:40:23 -0500

    users retrieved on login

--- client/src/routes/Users.js
+++ client/src/routes/Users.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import axios from 'axios';
+import required from '../auth/required';
 class Users extends React.Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount() {
+        axios.get('/')
+        .then(res => {
+            this.setState({ users: res.data.users })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
     render() {
         return(
             <div>
-                Users
+                <h2>Users</h2>
+                <ul>
+                    { this.state.users.map(u =>
+                    <li key={u.id}>{ u.username }</li>) }
+                </ul>
             </div>
         )
     }
 }
 
-export default Users;
+export default required(Users);
